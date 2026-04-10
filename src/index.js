6230d9668c2bce4a68d9816491e6813a50f2604c
IlliaAfanasenco <ilia.biz2@gmail.com>
2026-04-10 16:36:12 +0200

    init server node20

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import express from "express"
+import cors from 'cors'
+import dotenv from 'dotenv'
+import pg from 'pg'
+
+dotenv.config()
+const app = express()
+app.use(cors())
+app.use(express.json())
