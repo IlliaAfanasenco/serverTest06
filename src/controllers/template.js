e2a118d9fe217a803bb5ecd34948dd2f801bb4c5
IlliaAfanasenco <ilia.biz2@gmail.com>
2026-04-13 17:02:09 +0200

    add preview template

--- src/controllers/template.js
+++ src/controllers/template.js
@@ -1,4 +1,5 @@
 import {createTemplate, getLatestTemplate, updateTemplateById} from "../models/template.js";
+import Handlebars from 'handlebars'
 
 export const saveTemplate = async (req, res) => {
     try {
@@ -29,8 +30,24 @@ export const updateTemplate = async (req, res) => {
 
         const template = await updateTemplateById(id, content)
         res.json(template)
-
     } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+}
+
+export const previewTemplate = async (req, res) =>{
+    try {
+        const {content, variables} = req.body
+
+        if (!content) return res.status(400).json({error: 'no content'})
+        if (!variables) return res.status(400).json({error: 'no variables'})
+
+const template = Handlebars.compile(content)
+        const html = template(variables || {})
+
+        return res.json({html})
+    }
+    catch (error) {
         res.status(500).json({error: error.message})
     }
 }
--- src/routes/template.js
+++ src/routes/template.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
-import {fetchTemplate, saveTemplate, updateTemplate} from "../controllers/template.js";
+import {fetchTemplate, previewTemplate, saveTemplate, updateTemplate} from "../controllers/template.js";
 
 const templateRouter = Router()
 
 templateRouter.get('/', fetchTemplate)
 templateRouter.post('/', saveTemplate)
 templateRouter.put('/:id', updateTemplate)
+templateRouter.post('/preview', previewTemplate)
 
 export default templateRouter
